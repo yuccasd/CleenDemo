533c3e5bd87d79dcf9698b8861e7014fb9ecdc58
yuccasd <july@julydemacbook-pro.local>
2016-12-24 17:51:30 +0800

    init

---
+++ Cleen/Cleen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cleen
+//
+//  Created by July on 2016/12/24.
+//  Copyright © 2016年 yuccasd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
